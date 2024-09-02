a45faec75b496735459fa8ceebbfe661fbcfeef8
Roshan <luoshen1997@gmail.com>
2024-09-02 21:04:12 +0800

    Revert "change interrupt time"
    This reverts commit d570cec7a7b3e28f7fd1ddc19fa152bd46ad084d.

--- crates/blockchain-tree/src/chain.rs
+++ crates/blockchain-tree/src/chain.rs
@@ -263,6 +263,10 @@ impl AppendableChain {
             vec![snapshot.unwrap_or_default()],
         );
 
+        // stop the prefetch task.
+        #[cfg(feature = "prefetch")]
+        let _ = interrupt_tx.send(());
+
         // check state root if the block extends the canonical chain __and__ if state root
         // validation was requested.
         if block_validation_kind.is_exhaustive() {
@@ -295,17 +299,8 @@ impl AppendableChain {
                 "Validated state root"
             );
 
-            // stop the prefetch task.
-            #[cfg(feature = "prefetch")]
-            let _ = interrupt_tx.send(());
-
             Ok((initial_execution_outcome, trie_updates))
         } else {
-
-            // stop the prefetch task.
-            #[cfg(feature = "prefetch")]
-            let _ = interrupt_tx.send(());
-
             Ok((initial_execution_outcome, None))
         }
     }
